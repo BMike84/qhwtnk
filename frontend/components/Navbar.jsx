6a232382e36e93659b42253d0cd63e6d69dd722e
Michael Buffone <m_buffone@hotmail.com>
2023-03-01 22:08:27 -0500

    changed navbar styling will probably make more changes

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { images } from "@/constants/index";
-
+import { motion } from "framer-motion";
 import { IoMdHeartDislike } from "react-icons/io";
 import { FaHandHoldingHeart } from "react-icons/fa";
 
@@ -23,99 +23,109 @@ const navLinks = [
     link: "/#contact",
     title: "Contact",
   },
+  {
+    link: "/#book",
+    title: "Book Now",
+  },
 ];
 
 const Navbar = () => {
-  // open close menu for navbar mobile
-  const [nav, setNav] = useState(false);
-  //change navbar menu on scroll
-  const [color, setColor] = useState("");
-  const [textColor, setTextColor] = useState("");
-  const [logoColor, setLogoColor] = useState(images.logo_white);
-  const [borderColor, setBorderColor] = useState("");
-  // open close menu for navbar mobile
-  const handleNav = () => {
-    setNav(!nav);
+  const [toggle, setToggle] = useState(false);
+
+  const [navColor, setNavColor] = useState(false);
+  const [logoColor, setLogoColor] = useState(false);
+  const [buttonColor, setButtonColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setNavColor(true);
+      setLogoColor(true);
+      setButtonColor(true);
+    } else {
+      setNavColor(false);
+      setLogoColor(false);
+      setButtonColor(false);
+    }
   };
-  // change navbar menu on scroll
-  useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 50) {
-        setColor("#ffffff");
-        setTextColor("#000000");
-        setLogoColor(images.logo_black);
-        setBorderColor("#000000");
-      } else {
-        setColor("transparent");
-        setTextColor("#ffffff");
-        setLogoColor(images.logo_white);
-        setBorderColor("#ffffff");
-      }
-    };
-    window.addEventListener("scroll", changeColor);
-  }, []);
+
+  window.addEventListener("scroll", changeColor);
 
   return (
     <nav
-      style={{ backgroundColor: `${color}` }}
-      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+      className={
+        navColor
+          ? "fixed left-0 top-0 w-full z-10 ease-in duration-300 bg-white  text-black font-bold flex justify-between items-center py-4 px-8 "
+          : "fixed left-0 top-0 w-full z-10 ease-in duration-300  text-white font-bold flex justify-between items-center py-4 px-8 "
+      }
     >
-      <div className="max-w-[1440px]  flex justify-between items-center px-7 py-4 text-white">
+      <div className="app__navbar-logo">
         <Link href="/" scroll={true} className="text-center">
-          <Image src={logoColor} width={200} height={200} alt="logo" />
+          {logoColor ? (
+            <Image
+              src={images.logo_black}
+              width={200}
+              height={200}
+              alt="logo"
+            />
+          ) : (
+            <Image
+              src={images.logo_white}
+              width={200}
+              height={200}
+              alt="logo"
+            />
+          )}
         </Link>
-        <ul
-          style={{ color: `${textColor}` }}
-          className="hidden sm:flex font-bold"
-        >
-          {navLinks.map((item, index) => (
-            <li key={`${item}` + index} className="pl-5  hover:text-sky-500 ">
-              <Link
-                id="anchor"
-                href={item.link}
-                scroll={false}
-                className="linkTag"
-              >
+      </div>
+      <ul className="hidden flex-1 justify-center items-center gap-5 md:flex">
+        {navLinks.map((item) =>
+          item.title === "Book Now" ? (
+            <li key={`link-${item.title}`}>
+              <Link className="hidden" href={item.link}>
                 {item.title}
               </Link>
             </li>
-          ))}
-        </ul>
-        <button
-          className="hidden lg:block z-10 px-8 py-[0.40rem] border ease-in-out font-bold duration-300 rounded-3xl hover:bg-sky-500"
-          style={{ color: `${textColor}`, border: `2px solid ${borderColor}` }}
-        >
-          Book Now
-        </button>
-        {/* Mobile Button */}
-        <div onClick={handleNav} className="block sm:hidden z-10">
-          {nav ? (
-            <IoMdHeartDislike size={20} />
           ) : (
-            <FaHandHoldingHeart size={20} style={{ color: `${textColor}` }} />
-          )}
-        </div>
-        {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
-              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
-          }
-        >
-          <ul className="flex flex-col">
-            {navLinks.map((item, index) => (
-              <li key={index} className="p-4 text-3xl hover:text-sky-500">
-                <Link onClick={handleNav} href={item.link}>
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-            <button className="block text-2xl px-7 py-[.40rem] mt-3 border ease-in-out duration-300 rounded-3xl">
-              Book Now
-            </button>
-          </ul>
-        </div>
+            <li key={`link-${item.title}`}>
+              <Link href={item.link}>{item.title}</Link>
+            </li>
+          )
+        )}
+      </ul>
+      <Link
+        href="/"
+        className={
+          buttonColor
+            ? "hidden md:block border-2  border-black z-10 px-3 py-[0.33rem] ease-in-out font-bold duration-300 rounded-3xl"
+            : "hidden md:block border-2 z-10 px-3 py-[0.33rem] ease-in-out font-bold duration-300 rounded-3xl"
+        }
+      >
+        Book Now
+      </Link>
+
+      <div className="relative md:hidden">
+        <FaHandHoldingHeart onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <>
+            <motion.div
+              whileInView={{ x: [200, 0] }}
+              transition={{ duration: 0.85, ease: "easeOut" }}
+              className="fixed top-0 right-0 bottom-0 z-[5] p-4 w-[80%] h-screen flex justify-end items-end flex-col bg-black"
+            >
+              <IoMdHeartDislike onClick={() => setToggle(false)} />
+              <ul className="h-full w-full">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="m-4">
+                    <Link href={item.link} onClick={() => setToggle(false)}>
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          </>
+        )}
       </div>
     </nav>
   );
