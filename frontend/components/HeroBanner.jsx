470725e1e99fa2e94afa5ce8d524404012bb9278
Michael Buffone <m_buffone@hotmail.com>
2023-03-03 20:40:30 -0500

    seeing if it fixed on mobile doesnt show in chrome tools but mobile has issues

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -6,7 +6,7 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div
       id="home"
-      className="relative flex items-center justify-center h-[620px] lg:h-screen  bg-center bg-cover custom-img"
+      className="relative flex items-center justify-center h-screen  bg-center bg-cover custom-img"
     >
       <div className="absolute top-0 left-0 right-0 bottom-0  bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] flex lg:justify-center  lg:mb-0  lg:items-center flex-col w-full content-center lg:mt-10">
