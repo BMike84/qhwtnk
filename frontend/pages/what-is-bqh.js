ef71c261678238759fa127924cbf8a7f79aaa948
Michael Buffone <m_buffone@hotmail.com>
2023-03-07 16:25:20 -0500

    added first what is bqh page

---
+++ frontend/pages/what-is-bqh.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import WBQH from "@/components/WhatisBqh/WBQH";
+
+const whatIsBqh = () => {
+  return <WBQH />;
+};
+
+export default whatIsBqh;
