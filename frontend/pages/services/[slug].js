34aa02454d3577594e5fec89a7fa8d6f16dbf2e7
Michael Buffone <m_buffone@hotmail.com>
2023-03-17 19:12:34 -0400

    changed image size mobile service slug

--- frontend/pages/services/[slug].js
+++ frontend/pages/services/[slug].js
@@ -39,7 +39,7 @@ const ProductDetails = ({ product, products }) => {
                 <img
                   alt="product image"
                   src={urlFor(image && image[index])}
-                  className="rounded-lg w-[80%] h-[250px] mdSm:w-2/3 mdSm:h-[330px] md:h-[400px]  lg:h-[500px] lg:w-full"
+                  className="rounded-lg w-full h-[250px] mdSm:w-2/3 mdSm:h-[330px] md:h-[400px]  lg:h-[500px] lg:w-full"
                 />
                 {youtubeString?.length === 1
                   ? youtubeString.map((video) => (
